1ed8bc81165288fd5d5b00257c215cf0666c7f21
Ferjade <ferjadecatacutan@gmail.com>
2022-11-02 20:32:25 +0800

    changed navbar directory

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 export default function Navbar(){
     return( 
     <nav className="nav">
-        <Link to="/"><Image/></Link>
+        <Link to="/DevHub"><Image/></Link>
         <ul>
             <CustomLink to="/discover">Discover</CustomLink>
             <CustomLink to="/contact">Contact</CustomLink>
